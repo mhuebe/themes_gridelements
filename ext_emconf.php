7831714cbe547c19f24bb257e251034adf5fb29f
Michael Hübe <michael.huebe@web.de>
2017-01-27 00:26:24 +0100

    Changed dependency versions

--- ext_emconf.php
+++ ext_emconf.php
@@ -34,9 +34,9 @@ $EM_CONF[$_EXTKEY] = array(
 	'CGLcompliance_note' => '',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '7.6.0-7.6.99',
-			'themes' => '7.0.0-7.99.99',
-			'gridelements' => '7.0.0-7.99.99',
+			'typo3' => '7.6.0-',
+			'themes' => '7.0.0-',
+			'gridelements' => '7.0.0-',
 		),
 		'conflicts' => array(
 		),
